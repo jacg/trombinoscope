454805b59c6765f27a2e822509b86307a35cf798
Jacek Generowicz <jacg@my-post-office.net>
2024-05-24 11:05:13 +0200

    Bootstrap project with Dioxus example

---
+++ flake.nix
@@ -0,0 +1,72 @@
+# The core functionality is provided here using flakes. Legacy support for
+# `nix-shell` is provided by a wrapper in `shell.nix`.
+
+{
+  description = "Class photo list development environment";
+
+  inputs = {
+
+    # Version pinning is managed in flake.lock. Upgrading can be done with
+    # something like
+    #
+    #    nix flake lock --update-input nixpkgs
+
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-24.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils .url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+
+    # Option 1: try to support each default system
+    flake-utils.lib.eachDefaultSystem # NB Some packages in nixpkgs are not supported on some systems
+
+    # Option 2: try to support selected systems
+    # flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"]
+      (system:
+        let
+          pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                # ===== Specification of the rust toolchain to be used ====================
+                rust-overlay.overlays.default (final: prev:
+                  { rust-tools = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml; }
+                )
+              ];
+            };
+        in
+          {
+            devShell = pkgs.mkShell {
+              name = "my-rust-project";
+              buildInputs = [
+                pkgs.rust-tools
+                pkgs.cargo-nextest
+                pkgs.bacon
+                pkgs.just
+                pkgs.wasm-bindgen-cli
+                pkgs.openssl
+                pkgs.pkg-config
+                pkgs.dioxus-cli
+              ];
+              packages = [
+                pkgs.lolcat
+                pkgs.eza
+              ];
+              shellHook =
+                ''
+                  export PS1="rust devshell> "
+                  alias foo='cowsay Foo'
+                  alias bar='eza -l | lolcat'
+                  alias baz='cowsay What is the difference between buildIntputs and packages? | lolcat'
+                '';
+              # Requires "rust-src" to be present in components in ./rust-toolchain.toml
+              RUST_SRC_PATH = "${pkgs.rust-tools}/lib/rustlib/src/rust/library";
+            };
+          }
+      );
+}
---
+++ justfile
@@ -0,0 +1,4 @@
+# -*-Makefile-*-
+
+serve PORT='8080':
+     dx serve --hot-reload --port {{PORT}}
