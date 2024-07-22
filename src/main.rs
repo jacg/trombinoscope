40adc37f3ba924a548fc570748b0cbdf51a11ba3
Jacek Generowicz <jacg@my-post-office.net>
2024-07-22 18:49:39 +0200

    Replace example with tablex

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,50 @@ use classlist::TypstWrapperWorld;
 
 fn main() {
     let content = r#"
-#import "@preview/polylux:0.3.1": *
-#import themes.simple: *
+#import "@preview/tablex:0.0.8": tablex, rowspanx, colspanx
 
-#set page(paper: "presentation-16-9")
+#tablex(
+  columns: 4,
+  align: center + horizon,
+  auto-vlines: false,
 
-#show: simple-theme.with()
+  // indicate the first two rows are the header
+  // (in case we need to eventually
+  // enable repeating the header across pages)
+  header-rows: 2,
 
-#title-slide[
-= Hello, World!
-A document (+ `polylux` library) rendered with `Typst`!
-]"#
+  // color the last column's cells
+  // based on the written number
+  map-cells: cell => {
+    if cell.x == 3 and cell.y > 1 {
+      cell.content = {
+        let value = int(cell.content.text)
+        let text-color = if value < 10 {
+          red.lighten(30%)
+        } else if value < 15 {
+          yellow.darken(13%)
+        } else {
+          green
+        }
+        set text(text-color)
+        strong(cell.content)
+      }
+    }
+    cell
+  },
+
+  /* --- header --- */
+  rowspanx(2)[*Username*], colspanx(2)[*Data*], (), rowspanx(2)[*Score*],
+  (),                 [*Location*], [*Height*], (),
+  /* -------------- */
+
+  [John], [Second St.], [180 cm], [5],
+  [Wally], [Third Av.], [160 cm], [10],
+  [Jason], [Some St.], [150 cm], [15],
+  [Robert], [123 Av.], [190 cm], [20],
+  [Other], [Unknown St.], [170 cm], [25],
+)
+"#
     .to_owned();
 
     // Create world with content.
