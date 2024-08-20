da9ab68a02241cf766b569c7252161476db959e8
Jacek Generowicz <jacg@my-post-office.net>
2024-08-21 00:57:25 +0200

    Single item per person in typst trombinoscope table

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,6 @@ fn main() {
         true)
     };
 
-
     let items = find_jpgs_in_dir(&class_dir)
         .iter()
         .filter_map(path_to_item)
@@ -134,42 +133,64 @@ fn labels_typst_src(items: &[Item], class_dir: impl AsRef<Path>) -> String {
 }
 
 fn trombi_typst_src(items: &[Item], class_dir: impl AsRef<Path>) -> String {
-    let pic  = |i: &Item| { format!("\n    pic(\"{img}\"),", img=i.image.display()) };
-    let name = |i: &Item| {
-        let Name { given, family } = &i.name;
-        let family = family.to_uppercase();
-        format!("\n    label([{given}], [{family}]),")
-    };
 
-    let make_row = |range: std::ops::Range<usize>| {
-        let len = items.len();
-        if len <= range.start { ("".into(), "".into()) } else {
-
-            let npad = if len > range.end { 0 } else { range.end - len };
-            let (lo, hi) = (range.start.min(len), range.end.min(len));
-
-            (
-                items[lo..hi].iter().map(pic ).chain(vec!["[],".into(); npad]).collect::<Vec<_>>().join(""),
-                items[lo..hi].iter().map(name)                                .collect::<Vec<_>>().join("")
-            )
-        }
-    };
-
-    let w = 6;
-    let (row_1_pics, row_1_names)  = make_row(  0..w*1);
-    let (row_2_pics, row_2_names)  = make_row(w*1..w*2);
-    let (row_3_pics, row_3_names)  = make_row(w*2..w*3);
-    let (row_4_pics, row_4_names)  = make_row(w*3..w*4);
-
-    let table = format!(r#"
-    {row_1_pics} {row_1_names}
-    {row_2_pics} {row_2_names}
-    {row_3_pics} {row_3_names}
-    {row_4_pics} {row_4_names}
-"#);
+    let table_items = items
+        .iter()
+        .map(|Item { image, name: Name { given, family } }| {
+            format!("    item([{given}], [{family}], \"{image}\")", image=image.display())
+        })
+        .collect::<Vec<_>>()
+        .join(",\n");
 
     let class = class_from_dir(&class_dir);
-    format!("{header}{table})", header = header(&class))
+
+    format!(r#"#set page(
+  paper: "a4",
+  margin: (top: 10mm, bottom: 4mm, left: 5mm, right: 5mm),
+)
+
+#let colG = rgb(150,0,0)
+#let colF = rgb(0,0,150)
+
+#align(center, text([CLASSE {class}], size: 50pt))
+
+#v(-8mm) // TODO find sensible way of reducing space before table
+
+#let pic(path) = image(path, width: 100%)
+
+#let label(given, family) = [
+    #text(given , stroke: none, fill: colG) #h(1mm)
+    #text(family, stroke: none, fill: colF)
+]
+
+#let n_columns = 6
+
+#let item(given, family, path) = {{
+    set rect(
+        width: 200mm / n_columns,
+        inset: 5pt,
+        stroke: none,
+        height: 10mm,
+    )
+
+    stack(
+        dir: ttb,
+        rect(pic(path), height: 45mm),
+        rect(align(bottom, text(stroke: none, fill: colG,        given  ))),
+        rect(align(top   , text(stroke: none, fill: colF, upper[#family])),
+             stroke: (bottom: 0.5pt + gray)),
+    )
+}}
+
+#table(
+    columns: n_columns,
+    align: center + horizon,
+    stroke: none,
+
+{table_items}
+)
+"#)
+
 }
 
 fn class_from_dir(dir: impl AsRef<Path>) -> String {
@@ -197,31 +218,6 @@ fn family_given(l: &Item, r: &Item) -> Ordering {
             }
         }
     }
-}
-
-fn header(classe: &str) -> String {
-    format!(r#"#set page(
-  paper: "a4",
-  margin: (top: 10mm, bottom: 4mm, left: 5mm, right: 5mm),
-)
-
-#let colG = rgb(150,0,0)
-#let colF = rgb(0,0,150)
-
-#align(center, text([CLASSE {classe}], size: 50pt))
-
-#v(-8mm) // TODO find sensible way of reducing space before table
-
-#let pic(path) = image(path, width: 100%)
-
-#let label(given, family) = [
-    #text(given , stroke: none, fill: colG) #h(1mm)
-    #text(family, stroke: none, fill: colF)
-]
-
-#table(
-    columns: 6,
-    align: center + horizon,"#)
 }
 
 fn find_jpgs_in_dir(dir: impl AsRef<Path>) -> Vec<PathBuf> {
