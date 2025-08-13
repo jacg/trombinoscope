3f0d076a7fc5f471d1aeab2e6e565aadd2ee94d6
Jacek Generowicz <jacg@my-post-office.net>
2025-08-13 12:25:02 +0200

    Bump nixos 24.05 -> 25.05

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     #
     #    nix flake lock --update-input nixpkgs
 
-    nixpkgs     .url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-25.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils .url = "github:numtide/flake-utils";
     flake-compat = {
